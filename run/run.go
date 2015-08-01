fbdd2dc8d84c721df6ec266b0217d031bfdc0c4c
Adam Tulinius <atu@dbc.dk>
2015-08-01 20:00:25 +0200

    fix width of percent-success in statusline

--- run/run.go
+++ run/run.go
@@ -66,7 +66,7 @@ func printStatusHeader() {
 func printStatus(tStart time.Time, sent int, done int, successful int, lag time.Duration, skipped int) {
 	runTime := int(time.Now().Sub(tStart).Seconds())
 	nanoLag := float64(lag.Nanoseconds()) / float64(1000000)
-	fmt.Printf("\r%6ds  %12.3f   %8d  %8d  %8d  %8d %3.2f%%  %8d", runTime, nanoLag, sent, done, sent-done, successful, 100*float64(successful)/float64(done), skipped)
+	fmt.Printf("\r%6ds  %12.3f   %8d  %8d  %8d  %8d %6.2f%%  %8d", runTime, nanoLag, sent, done, sent-done, successful, 100*float64(successful)/float64(done), skipped)
 }
 
 func Run(prefix *string, flood *bool, speedup *float64, rampUpSecs *int) {
